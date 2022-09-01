f78a3980a3de0d1c317e08a49fc2a72420be174d
AnaTorres7 <anaflaviatorresc@gmail.com>
2022-09-01 17:22:47 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ALURAFLIX
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/AnaFlavia/pen/ExZPQee](https://codepen.io/AnaFlavia/pen/ExZPQee).
+
---
+++ src/script.js
@@ -0,0 +1,90 @@
+var boxAnimacao = []
+boxAnimacao.push("https://m.media-amazon.com/images/M/MV5BMTU1MTAwMjk1NF5BMl5BanBnXkFtZTcwMDI5NDc4Ng@@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxAnimacao.push("https://m.media-amazon.com/images/M/MV5BMTc4NjEyODE1OV5BMl5BanBnXkFtZTcwMjYzNTkxNA@@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxAnimacao.push("https://m.media-amazon.com/images/M/MV5BMTU2MDY3MzAzMl5BMl5BanBnXkFtZTcwMTg0NjM5NA@@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxAnimacao.push("https://m.media-amazon.com/images/M/MV5BYjQ5NjM0Y2YtNjZkNC00ZDhkLWJjMWItN2QyNzFkMDE3ZjAxXkEyXkFqcGdeQXVyODIxMzk5NjA@._V1_UY268_CR3,0,182,268_AL_.jpg")
+boxAnimacao.push("https://m.media-amazon.com/images/M/MV5BODkxNGQ1NWYtNzg0Ny00Yjg3LThmZTItMjE2YjhmZTQ0ODY5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+var boxComedia = []
+boxComedia.push("https://m.media-amazon.com/images/M/MV5BMTgxMzgwMDM4OF5BMl5BanBnXkFtZTgwNjk4NjcyMTE@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxComedia.push("https://m.media-amazon.com/images/M/MV5BMTY3OTg2OTM3OV5BMl5BanBnXkFtZTYwNzY5OTA3._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxComedia.push("https://m.media-amazon.com/images/M/MV5BMTcyMjdlYmUtNjQ1Zi00YTg3LTliNTgtZmNkOTRmYzEzYTMzXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxComedia.push("https://m.media-amazon.com/images/M/MV5BYWE3YWM5NDQtN2ZlZi00ZWY0LTg4M2QtMGNjMjEwNzAzY2M2XkEyXkFqcGdeQXVyMTkzODUwNzk@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxComedia.push("https://m.media-amazon.com/images/M/MV5BODQ0NDhjYWItYTMxZi00NTk2LWIzNDEtOWZiYWYxZjc2MTgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+var boxDrama = []
+boxDrama.push("https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxDrama.push("https://m.media-amazon.com/images/M/MV5BMjQ1NjM3MTUxNV5BMl5BanBnXkFtZTgwMDc5MTY5OTE@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxDrama.push("https://m.media-amazon.com/images/M/MV5BOGE3N2QxN2YtM2ZlNS00MWIyLWE1NDAtYWFlN2FiYjY1MjczXkEyXkFqcGdeQXVyOTUwNzc0ODc@._V1_UY268_CR3,0,182,268_AL_.jpg")
+boxDrama.push("https://m.media-amazon.com/images/M/MV5BNzE4NDg5OWMtMzg3NC00ZDRjLTllMDMtZTRjNWZmNjBmMGZlXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR3,0,182,268_AL_.jpg")
+boxDrama.push("https://m.media-amazon.com/images/M/MV5BMTQ5NjQ0NDI3NF5BMl5BanBnXkFtZTcwNDI0MjEzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+var boxDocumentario = []
+boxDocumentario.push("https://m.media-amazon.com/images/M/MV5BNDE2NGVmOWItYWI2NC00ZDcxLTk1NmQtYTE5M2U0MmIyYWM1XkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxDocumentario.push("https://m.media-amazon.com/images/M/MV5BNDQyMDMxNzUtMTkwMC00ZTk1LWIxYzYtYTBlZGQ3Yjg2Mjc0XkEyXkFqcGdeQXVyNjEwNTM2Mzc@._V1_UY268_CR4,0,182,268_AL_.jpg")
+boxDocumentario.push("https://m.media-amazon.com/images/M/MV5BMWM0ZTc1MmYtYmRiMi00MDcxLWI4N2EtNTk3ZDk0Y2YyZmQxXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UY268_CR4,0,182,268_AL_.jpg")
+boxDocumentario.push("https://m.media-amazon.com/images/M/MV5BN2I1ZjA5YjQtYmQ0ZS00ZmE1LTk1ZjktNTQ5ODIzY2JiZDdhXkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxDocumentario.push("https://m.media-amazon.com/images/M/MV5BODgxNmM5ODctN2RhZi00ODZhLWFmNGMtNzVmNmRkZmVhMTk3XkEyXkFqcGdeQXVyMjExNjgyMTc@._V1_UY268_CR43,0,182,268_AL_.jpg")
+
+var boxFiccaoC = []
+boxFiccaoC.push("https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxFiccaoC.push("https://m.media-amazon.com/images/M/MV5BMTQ2ODFlMDAtNzdhOC00ZDYzLWE3YTMtNDU4ZGFmZmJmYTczXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxFiccaoC.push("https://m.media-amazon.com/images/M/MV5BNzA1Njg4NzYxOV5BMl5BanBnXkFtZTgwODk5NjU3MzI@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxFiccaoC.push("https://m.media-amazon.com/images/M/MV5BNWU2NzEyMDYtM2MyOS00OGM3LWFkNzAtMzRiNzE2ZjU5ZTljXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxFiccaoC.push("https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+var boxRomance = []
+boxRomance.push("https://m.media-amazon.com/images/M/MV5BMTU0NzQzODUzNl5BMl5BanBnXkFtZTgwMjc5NTYxMTE@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxRomance.push("https://m.media-amazon.com/images/M/MV5BNWMzZTE4NTYtOGRiNi00YzQ2LWJiNjctMzk1NDU4NmY5ZGQwXkEyXkFqcGdeQXVyODIyMzA4NDc@._V1_UY268_CR0,0,182,268_AL_.jpg")
+boxRomance.push("https://m.media-amazon.com/images/M/MV5BMTc3OTg3MDUwN15BMl5BanBnXkFtZTgwMTAwMTkxNDE@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxRomance.push("https://m.media-amazon.com/images/M/MV5BYzI1YTFlNDEtMjE1MC00Y2MyLTkyMTItODliNDNhNTQ4YzdiXkEyXkFqcGdeQXVyMjMxMjA5ODM@._V1_UY268_CR0,0,182,268_AL_.jpg")
+boxRomance.push("https://m.media-amazon.com/images/M/MV5BNzVmMjJlN2MtNWQ4Ny00Zjc2LWJjYTgtYjJiNGM5MTM1ZTlkXkEyXkFqcGdeQXVyMjM4NTM5NDY@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+var boxHeroi = []
+boxHeroi.push("https://m.media-amazon.com/images/M/MV5BMTYzODQzYjQtNTczNC00MzZhLTg1ZWYtZDUxYmQ3ZTY4NzA1XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxHeroi.push("https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxHeroi.push("https://m.media-amazon.com/images/M/MV5BMTY5OTU0OTc2NV5BMl5BanBnXkFtZTcwMzU4MDcyMQ@@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxHeroi.push("https://m.media-amazon.com/images/M/MV5BYWVhZjZkYTItOGIwYS00NmRkLWJlYjctMWM0ZjFmMDU4ZjEzXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg")
+boxHeroi.push("https://m.media-amazon.com/images/M/MV5BMTAzMzI0NTMzNDBeQTJeQWpwZ15BbWU3MDM3NTAyOTM@._V1_UX182_CR0,0,182,268_AL_.jpg")
+
+for(i = 0; i < boxAnimacao.length; i++){
+  var img = document.createElement("IMG")
+  img.src = boxAnimacao[i]
+  document.getElementById("animacao").appendChild(img)
+}
+
+for(i = 0; i < boxComedia.length; i++){
+  var img = document.createElement("IMG")
+  img.src = boxComedia[i]
+  document.getElementById("comedia").appendChild(img)
+}
+
+for(i = 0; i < boxDrama.length; i++){
+  var img = document.createElement("IMG")
+  img.src = boxDrama[i]
+  document.getElementById("drama").appendChild(img)
+}
+
+for(i = 0; i < boxDocumentario.length; i++){
+  var img = document.createElement("IMG")
+  img.src = boxDocumentario[i]
+  document.getElementById("documentario").appendChild(img)
+}
+
+for(i = 0; i < boxFiccaoC.length; i++){
+  var img = document.createElement("IMG")
+  img.src = boxFiccaoC[i]
+  document.getElementById("ficcaoC").appendChild(img)
+}
+
+for(i = 0; i < boxRomance.length; i++){
+  var img = document.createElement("IMG")
+  img.src = boxRomance[i]
+  document.getElementById("romance").appendChild(img)
+}
+
+for(i = 0; i < boxHeroi.length; i++){
+  var img = document.createElement("IMG")
+  img.src = boxHeroi[i]
+  document.getElementById("heroi").appendChild(img)
+}
